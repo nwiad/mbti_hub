1860e9901942de41cec5b88b2c5f397fdc382b70
daiweinan.thu <daiweinan.thu@bytedance.com>
2025-04-26 15:22:05 +0000

    fix path

--- script/run_sft_qwen_peft_multi.sh
+++ script/run_sft_qwen_peft_multi.sh
@@ -1,19 +1,14 @@
-# Tested with 2 & 4 GPUs
-
-# set -x
-
-
 #!/usr/bin/env bash
 set -euxo pipefail
 
 timestamp=$(date +"%m%d_%H%M")
-nproc_per_node=4
-save_path="../ckpt"
+nproc_per_node=8
 model_name="Qwen2.5-7B-Instruct"
-model_path="/yeesuanAI08/LLM/${model_name}"
+model_path="hdfs://harunava/home/byte_data_seed_azure/alphaseed/daiweinan/models/Qwen2.5-7B-Instruct"
 project_name="dwn-verl"
 exp_name="mbti-sft_${model_name}_${timestamp}"
 epochnum=2
+save_path="/mnt/hdfs/length_scaling/experiments/mbti/multi_profile/${exp_name}"
 
 echo "Usage: sft_run_qwen_peft.sh" # 打印参数
 echo "Parameters:"
@@ -21,11 +16,9 @@ echo "nproc_per_node: ${nproc_per_node}"
 echo "model_path: ${model_path}"
 echo "save_path: ${save_path}"
 
-export CUDA_VISIBLE_DEVICES=0,4,5,6
 
-
-train_files="../data/Machine_Mindset_MBTI_dataset/en/multi_profile/train_with_token.parquet"
-val_files="../data/Machine_Mindset_MBTI_dataset/en/multi_profile/test_with_token.parquet"
+train_files="/opt/tiger/dwn-opensource-verl/mbti/mbti_data/train_with_token.parquet"
+val_files="/opt/tiger/dwn-opensource-verl/mbti/mbti_data/test_with_token.parquet"
 
 torchrun --standalone --nnodes=1 --nproc_per_node=${nproc_per_node} \
      -m verl.trainer.fsdp_sft_trainer_multi \
@@ -33,15 +26,15 @@ torchrun --standalone --nnodes=1 --nproc_per_node=${nproc_per_node} \
      data.val_files=${val_files} \
      data.prompt_key="instruction" \
      data.response_key="output" \
-     +data.prompt_dict_keys=null \
-     +data.response_dict_keys=null \
+     data.prompt_dict_keys=null \
+     data.response_dict_keys=null \
      optim.lr=1e-4 \
-     data.micro_batch_size_per_gpu=2 \
+     data.micro_batch_size_per_gpu=4 \
      model.partial_pretrain=${model_path} \
-     trainer.default_local_dir=${save_path}/multi_profile/ \
+     trainer.default_local_dir=${save_path} \
      trainer.project_name=${project_name} \
      trainer.experiment_name=${exp_name} \
-     trainer.logger=['console'] \
+     trainer.logger=['console','wandb'] \
      trainer.total_epochs=${epochnum} \
      trainer.default_hdfs_dir=null \
      model.lora_rank=8 \
